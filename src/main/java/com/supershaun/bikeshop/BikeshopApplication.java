6c715bb8c99af43ac19c7d417fd5a92d67cbf264
SuperMistral <mediamart37@gmail.com>
2022-05-08 15:25:22 +0300

    Initial commit

---
+++ src/main/java/com/supershaun/bikeshop/BikeshopApplication.java
@@ -0,0 +1,13 @@
+package com.supershaun.bikeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeshopApplication.class, args);
+    }
+
+}
